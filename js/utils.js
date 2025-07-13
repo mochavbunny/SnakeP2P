aef1fe9c573a319deb394b5d7077f93ca40b7fb1
user <148361551+edot9241@users.noreply.github.com>
2025-07-13 16:07:33 +0200

    Utils make spacing consistent

--- js/utils.js
+++ js/utils.js
@@ -6,9 +6,11 @@ class Utils {
         "down":  "up",
     }
 
+
     static areOppositeDirections(d0, d1) {
         return this.#oppositeDirections[d0] === d1;
     }
+
 
     static getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
