748efcfae3fce0667a0c3efefe068a26c4297fe6
mochavbunny <>
2025-07-13 16:31:08 +0100

    Have different colours for snake and apple

--- js/display.js
+++ js/display.js
@@ -1,20 +1,24 @@
 "use strict";
 
 class Display {
-    static backgroundColor = "#e0e0e0";
-    static blankSquareColor = "#c0c0c0";
-    static filledSquareColor = "#505050";
+    static #backgroundColor = "#e0e0e0";
+    static #blankSquareColor = "#c0c0c0";
+    static #player1BodyColor = "#00FF00";
+    static #player1HeadColor = "#00D700";
+    static #player2BodyColor = "#00EFFF";
+    static #player2HeadColor = "#00CFFF";
+    static #appleColor = "#FF0000";
 
-    static squareMarginPercent = 5;
+    static #squareMarginPercent = 5;
 
-    static defaultWidth = 800;
-    static defaultHeight = 600;
+    static #defaultWidth = 800;
+    static #defaultHeight = 600;
 
 
-    constructor(
+    static initialize(
         canvasID = "game-canvas",
-        width = Display.defaultWidth,
-        height = Display.defaultHeight
+        width = this.#defaultWidth,
+        height = this.#defaultHeight
     ) {
         const canvas = document.getElementById(canvasID);
         canvas.width = width;
@@ -26,17 +30,19 @@ class Display {
 
         this.squareWidth = this.width / Constants.columns;
         this.squareHeight = this.height / Constants.rows;
-        this.squareMarginX = this.squareWidth * (Display.squareMarginPercent / 100);
-        this.squareMarginY = this.squareHeight * (Display.squareMarginPercent / 100);
+        this.squareMarginX = this.squareWidth * (this.#squareMarginPercent / 100);
+        this.squareMarginY = this.squareHeight * (this.#squareMarginPercent / 100);
     }
 
 
-    draw(snake, apple) {
+    static draw(snake, apple) {
         this.clear();
         this.drawField();
 
-        if (typeof snake !== undefined) {
-            this.drawSnake(snake);
+        this.drawSnake(snake, this.#player1HeadColor, this.#player1BodyColor);
+
+        if (Game.multiplayer) {
+            this.drawSnake(snake, this.#player2HeadColor, this.#player2BodyColor);
         }
 
         if (typeof apple !== undefined) {
@@ -45,24 +51,24 @@ class Display {
     }
 
 
-    drawField() {
+    static drawField() {
         this.drawBackground();
 
         for (let row = 0; row < Constants.rows; row++) {
             for (let col = 0; col < Constants.columns; col++) {
-                this.drawSquare(col, row, Display.blankSquareColor);
+                this.drawSquare(col, row, this.#blankSquareColor);
             }
         }
     }
 
 
-    drawBackground() {
-        this.ctx.fillStyle = Display.backgroundColor;
+    static drawBackground() {
+        this.ctx.fillStyle = this.#backgroundColor;
         this.ctx.fillRect(0, 0, this.width, this.height);
     }
 
 
-    drawSquare(col, row, color) {
+    static drawSquare(col, row, color) {
         let x = col * this.squareWidth + this.squareMarginX;
         let y = row * this.squareHeight + this.squareMarginY;
         let width = this.squareWidth - (this.squareMarginX * 2);
@@ -73,19 +79,20 @@ class Display {
     }
 
 
-    drawSnake(snake) {
-        snake.forEach(block => {
-            this.drawSquare(block[0], block[1], Display.filledSquareColor);
+    static drawSnake(snake, headColor, bodyColor) {
+        snake.forEach((block, index) => {
+            const snakeColor = (index === 0) ? headColor : bodyColor;
+            this.drawSquare(block[0], block[1], snakeColor);
         })
     }
 
 
-    drawApple(apple) {
-        this.drawSquare(apple[0], apple[1], Display.filledSquareColor);
+    static drawApple(apple) {
+        this.drawSquare(apple[0], apple[1], this.#appleColor);
     }
     
 
-    clear() {
+    static clear() {
         this.ctx.clearRect(0, 0, this.width, this.height);
     }
 }
