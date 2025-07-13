1d4dc827c7bf24bae2d57a195519b28f8b5c9a7d
user <148361551+edot9241@users.noreply.github.com>
2025-07-13 09:59:24 +0200

    Add utils.js

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+class Utils {
+    static #oppositeDirections = {
+        "right": "left",
+        "left":  "right",
+        "up":    "down",
+        "down":  "up",
+    }
+
+    static areOppositeDirections(d0, d1) {
+        return this.#oppositeDirections[d0] === d1;
+    }
+
+    static getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+}
